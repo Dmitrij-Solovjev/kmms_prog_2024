ddb988e146309ede530fba8825fd25e7ab0a007f
Dmitrij <dima_solovjev03@mail.ru>
2024-10-17 19:16:14 +0300

    удалено:       1010_Refactoring_Homework/helper.h 	изменено:      1010_Refactoring_Homework/task1.cpp

--- 1010_Refactoring_Homework/task1.cpp
+++ 1010_Refactoring_Homework/task1.cpp
@@ -1,52 +1,64 @@
-﻿#include <cmath>
 #include <iostream>
 
-#include "helper.h"
-
-#define MAX_ELEM 10
-#define NUM_ELEM 10
-
-void print_array(const char* const comment, const int *arr, const unsigned arr_len, const char* const separator = " "){
-   std::cout << comment << std::endl;
-
-   for (auto i = 0; i < arr_len; i++)
-      std::cout << arr[i] << separator;
-}
-
-void generate_array(const int *arr, const unsigned arr_len, const unsigned int max_elem){
-   for (auto i = 0; i < arr_len; i++)
-      arr[i] = rand() % (max_elem + 1);
-}
-
-int calculate_sum(const int *arr, const unsigned arr_len){
-   int sum = 0;
-
-   for (auto i = 0; i < arr_len; i++)
-      sum+=arr[i];
-
-   return sum;
-}
-
+size_t calculate_sum(const int *arr, const unsigned arr_len);
+void generate_array(int * const arr, const size_t arr_len, const size_t max_elem);
+void print_array(const char * const comment,  const int * const arr, const size_t arr_len, const char * const separator = " ");
 
 int main() {
-   auto str_arr_1 = {
-      "Первая последовательность",
-      "Вторая последовательность",
-      "Третья последовательность"};
-   auto str_arr_2 = {
-      "среднее первой последовательности",
-      "среднее первой последовательности",
-      "среднее первой последовательности"};
+   const size_t max_elem = 10;
+   const size_t num_elem = 10;
 
-   int arr[NUM_ELEM] = {0};
+   int arr[num_elem] = {0};
+   
+   const char *str_arr_1[3] = {
+      "Первая последовательность:",
+      "Вторая последовательность:",
+      "Третья последовательность:"};
+
+   const char *str_arr_2[3] = {
+      "среднее первой  последовательности: ",
+      "среднее второй  последовательности: ",
+      "среднее третьей последовательности: "};
 
    for (auto i = 0; i < 3; i++){
-      print_array(str_arr_1[i], arr, NUM_ELEM);
+      generate_array(arr, num_elem, max_elem);
 
-      const unsigned int sum = calculate_sum(arr, NUM_ELEM);
-
+      print_array(str_arr_1[i], arr, num_elem);
       
+      const size_t sum = calculate_sum(arr, num_elem);
+      float mean = (double)sum / num_elem;
+      
+      std::cout << str_arr_2[i] << mean << std::endl;
    }
 
     return 0;
 }
+
+
+size_t calculate_sum(const int *arr, const unsigned arr_len){
+   size_t sum = 0;
+
+   for (size_t i = 0; i < arr_len; i++){
+      sum+=arr[i];
+   }
+   
+   return sum;
+}
+
+void generate_array(int * const arr, const size_t arr_len, const size_t max_elem){
+   for (size_t i = 0; i < arr_len; i++){
+      arr[i] = rand() % (max_elem + 1);
+   }
+}
+
+void print_array(const char * const comment,  const int * const arr, const size_t arr_len, const char * const separator) {
+    std::cout << comment << std::endl;
+
+    for (size_t i = 0; i < arr_len; i++) {
+        std::cout << arr[i] << separator;
+    }
+
+    std::cout << std::endl;
+}
+
+
